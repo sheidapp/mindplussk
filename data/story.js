189d9d349528d9f4b0988482627a3629a1390098
NextMoveDigital <42180017+sheidapp@users.noreply.github.com>
2026-06-01 17:45:02 +0200

    Add files via upload

---
+++ data/story.js
@@ -0,0 +1,597 @@
+var story = {
+ "docName": "Slovakia_Mind-+NTH",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "showFigmaLinks": false,
+ "hideGallery": true,
+ "galleryPageColorsEnabled": false,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "Slovakia_Mind-+NTH",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "1:84",
+   "groupIndex": 0,
+   "title": "LP",
+   "image": "lp.png",
+   "index": 0,
+   "width": 375,
+   "height": 751,
+   "x": 403,
+   "y": 309,
+   "awidth": 375,
+   "aheight": 751,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Rectangle 3",
+     "rect": {
+      "x": 27,
+      "y": 427,
+      "width": 330,
+      "height": 60
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 240,
+      "y": 682,
+      "width": 40,
+      "height": 10
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 95,
+      "y": 682,
+      "width": 129,
+      "height": 10
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 95,
+      "y": 707,
+      "width": 193,
+      "height": 10
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "2:353",
+   "groupIndex": 0,
+   "title": "Homepage  - Train",
+   "image": "homepage-train.png",
+   "index": 1,
+   "width": 414,
+   "height": 885,
+   "x": 1243,
+   "y": 309,
+   "awidth": 414,
+   "aheight": 885,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Text",
+     "rect": {
+      "x": 36,
+      "y": 44,
+      "width": 340,
+      "height": 43
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "12:52",
+   "groupIndex": 0,
+   "title": "TC",
+   "image": "tc.png",
+   "index": 2,
+   "width": 414,
+   "height": 3468,
+   "x": 1684,
+   "y": 309,
+   "awidth": 414,
+   "aheight": 3468,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Text",
+     "rect": {
+      "x": 67,
+      "y": 46,
+      "width": 311,
+      "height": 30
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Back small",
+     "rect": {
+      "x": 46,
+      "y": 73,
+      "width": 11,
+      "height": 19
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 256,
+      "y": 3408,
+      "width": 40,
+      "height": 10
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 111,
+      "y": 3408,
+      "width": 129,
+      "height": 10
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 2,
+       "disableAutoScroll": true,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 111,
+      "y": 3433,
+      "width": 193,
+      "height": 10
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "12:71",
+   "groupIndex": 0,
+   "title": "Zásady ochrany osobných údajov",
+   "image": "zásady-ochrany-osobných-údajov.png",
+   "index": 3,
+   "width": 414,
+   "height": 2225,
+   "x": 2125,
+   "y": 309,
+   "awidth": 414,
+   "aheight": 2225,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Text",
+     "rect": {
+      "x": 67,
+      "y": 46,
+      "width": 311,
+      "height": 60
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Back small",
+     "rect": {
+      "x": 46,
+      "y": 73,
+      "width": 11,
+      "height": 19
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 256,
+      "y": 2154,
+      "width": 40,
+      "height": 10
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 111,
+      "y": 2154,
+      "width": 129,
+      "height": 10
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 111,
+      "y": 2179,
+      "width": 193,
+      "height": 10
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 3,
+       "disableAutoScroll": true,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "12:122",
+   "groupIndex": 0,
+   "title": "Contact Us",
+   "image": "contact-us.png",
+   "imageFixedLess": "fl/contact-us.png",
+   "index": 4,
+   "width": 319,
+   "height": 943,
+   "x": 2566,
+   "y": 309,
+   "awidth": 319,
+   "aheight": 943,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [
+    {
+     "constrains": {
+      "top": false,
+      "bottom": false,
+      "left": false,
+      "right": false,
+      "height": false,
+      "width": false
+     },
+     "x": -0.7705078125,
+     "y": 609.490234375,
+     "width": 328.2463684082031,
+     "height": 92.4637680053711,
+     "awidth": 328.2463684082031,
+     "aheight": 92.4637680053711,
+     "type": "float",
+     "index": 0,
+     "isFloat": true,
+     "links": [],
+     "image": "contact-us-0.png",
+     "mskH": 92.4637680053711,
+     "shadow": "0px -3.082125663757324px 7.7053141593933105px #187E8933"
+    },
+    {
+     "constrains": {
+      "top": false,
+      "bottom": false,
+      "left": false,
+      "right": false,
+      "height": false,
+      "width": false
+     },
+     "x": -0.7705078125,
+     "y": 609.490234375,
+     "width": 328.2463684082031,
+     "height": 92.4637680053711,
+     "awidth": 328.2463684082031,
+     "aheight": 92.4637680053711,
+     "type": "float",
+     "index": 1,
+     "isFloat": true,
+     "links": [],
+     "image": "contact-us-1.png",
+     "mskH": 92.4637680053711,
+     "shadow": "0px -3.082125663757324px 7.7053141593933105px #187E8933"
+    }
+   ],
+   "links": [
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 206,
+      "y": 870,
+      "width": 40,
+      "height": 10
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 4,
+       "disableAutoScroll": true,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 61,
+      "y": 870,
+      "width": 129,
+      "height": 10
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Important Information",
+     "rect": {
+      "x": 61,
+      "y": 895,
+      "width": 193,
+      "height": 10
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 5,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#1E1E1E",
+   "sections": []
+  }
+ ]
+}
